0efa2ddf06015ad6efe6a2d88beebca4955502a9
gordonbchen <gordonbchen@gmail.com>
2025-08-20 17:26:38 -0400

    add Fill.asm 04

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,72 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// nScreen = KBD - SCREEN
+//
+// LOOP
+//   color = 0
+//   if RAM[KBD] == 0: jump DRAW
+//   color = -1
+//
+//  DRAW
+//    i = nScreen
+//    addr = SCREEN
+//    DRAWLOOP
+//      RAM[addr] = color
+//      i--
+//      addr++
+//      if i > 0: jump DRAWLOOP
+//
+//  jump LOOP
+
+@KBD
+D=A
+@SCREEN
+D=D-A
+@nScreen
+M=D  // nScreen = KBD - SCREEN
+
+(LOOP)
+@color
+M=0  // color = 0
+
+@KBD
+D=M
+@DRAW
+D;JEQ  // if RAM[KBD] == 0: jump DRAW
+
+@color
+M=M-1  // color = -1
+
+(DRAW)
+@nScreen
+D=M
+@i
+M=D  // i = nScreen
+
+@SCREEN
+D=A
+@addr
+M=D  // addr = SCREEN
+
+(DRAWLOOP)
+@color
+D=M
+@addr
+A=M
+M=D  // RAM[addr] = color
+
+@i
+M=M-1  // i--
+
+@addr
+M=M+1  // addr++
+
+@i
+D=M
+@DRAWLOOP
+D;JGT  // if i > 0: jump DRAWLOOP
+
+@LOOP
+0;JMP  // jump LOOP
+
