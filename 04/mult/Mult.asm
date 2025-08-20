2340ef6b9e29cbd430d74ffabbe740358db3f485
gordonbchen <gordonbchen@gmail.com>
2025-08-20 12:04:02 -0400

    add 04 Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -8,5 +8,63 @@
 //
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+//
+// x = R0
+// y = R1
+// i = 0
+// product = 0
+// LOOP
+//   if i == x: jump STOP
+//   product += y
+//   i++
+//   jump LOOP
+// STOP
+//   R2 = product
+// END
 
-// Put your code here.
+@R0
+D=M
+@x
+M=D  // x = R0
+
+@R1
+D=M
+@y
+M=D  // y = R1
+
+@i
+M=0  // i = 0
+
+@product
+M=0  // product = 0
+
+(LOOP)
+@x
+D=M
+@i
+D=D-M  // D = x - i
+
+@STOP
+D;JEQ  // jump STOP if i == x
+
+@y
+D=M
+@product
+M=D+M  // product += y
+
+@i
+M=M+1  // i++
+
+@LOOP
+0;JMP  // jump LOOP
+
+(STOP)
+@product
+D=M
+@R2
+M=D  // R2 = product
+
+(END)
+@END
+0;JMP
+
